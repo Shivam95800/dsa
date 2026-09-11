5b6b80701ee0e12d372fe46238834415102b9145
Shivam Soni <shivam301102@gmail.com>
2026-09-11 08:09:11 +0530

    Time: 10 ms (6.02%), Space: 48.4 MB (25.99%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean searchMatrix(int[][] arr, int tar) {
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr[0].length; j++){
+                if(arr[i][j]== tar) return true;
+            }
+        }
+        return false;
+    }
+}
